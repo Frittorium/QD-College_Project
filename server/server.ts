c738dc11c77e0a976bb9463087f1f64cc3b9df81
Frittorium <f0rsakencrown@proton.me>
2026-07-26 22:28:27 +0545

    build time error fixes

--- server/server.ts
+++ server/server.ts
@@ -40,3 +40,5 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction)=>{
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
+
+export default app;
